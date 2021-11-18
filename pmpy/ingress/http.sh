86e41a757c6ae2c704d0e0435d5237f4b321f5fb
Arthur Tokarchuk <btmc@ya.ru>
2021-11-18 20:56:18 +0300

    fix

--- pmpy/ingress/http.sh
+++ pmpy/ingress/http.sh
@@ -1,10 +1,22 @@
-set -ea
+set -e
 
 : ${SCRIPTS_DIR:=$PMPY_SCRIPTS_DIR} \
   ${SCRIPTS_DIR:?}
 
-`jq '.|to_entries|.[]
-     |.key + @sh "=\(.value)"' -r`
+exec {ENV_FD}> `mktemp`
+
+rm `readlink \
+   /dev/fd/$ENV_FD`
+
+  >& $ENV_FD \
+jq '.|to_entries|.[]
+     |.key + @sh "=\(.value)"' -r
+
+
+set -a
+
+. /dev/fd/$ENV_FD
+
 
 > /dev/null \
 $SCRIPTS_DIR/process.sh &
