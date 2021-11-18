0ad1edaed4c79a01a0f0a9d6523c2eb70b958a28
Arthur Tokarchuk <btmc@ya.ru>
2021-11-18 17:27:31 +0300

    fix

--- pmpy/ingress/http.sh
+++ pmpy/ingress/http.sh
@@ -3,9 +3,10 @@ set -ea
 : ${SCRIPTS_DIR:=$PMPY_SCRIPTS_DIR} \
   ${SCRIPTS_DIR:?}
 
-jq '.|to_entries|.[]
-     |.key + @sh "=\(.value)"' -r | . /dev/stdin
+`jq '.|to_entries|.[]
+     |.key + @sh "=\(.value)"' -r`
 
+> /dev/null \
 $SCRIPTS_DIR/process.sh &
 
 echo 'Status: 204'
